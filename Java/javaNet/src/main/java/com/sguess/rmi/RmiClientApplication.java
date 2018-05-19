d95dcd7d0d2d5b8729e07748e351f4092cebdf91
daniel.pei <peixingzhou@sina.com>
2018-05-19 18:39:18 +0800

    update

---
+++ Java/javaNet/src/main/java/com/sguess/rmi/RmiClientApplication.java
@@ -0,0 +1,12 @@
+package com.sguess.rmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmiClientApplication.class, args);
+	}
+}
